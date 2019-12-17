0dc3cc06d44a4f36008e450259e794060bc10937
FanJups <jupsfan@gmail.com>
2019-12-17 10:08:40 +0100

    Adding test

--- src/test/java/basketball/vogt/WebsiteBackendApiApplicationTests.java
+++ src/test/java/basketball/vogt/WebsiteBackendApiApplicationTests.java
@@ -1,13 +1,22 @@
 package basketball.vogt;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import basketball.vogt.controller.HelloController;
 
 @SpringBootTest
 class WebsiteBackendApiApplicationTests {
 
+	@Autowired
+	private HelloController helloController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		
+		Assert.assertEquals("Hello World from Vogt Basketball",helloController.home() );
 	}
 
 }
