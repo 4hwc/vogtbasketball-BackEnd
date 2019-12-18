87869b208045da6aa793aba581b9897fb5387b8b
Fanon Jupkwo <jupsfan@gmail.com>
2019-12-18 21:51:33 +0100

    Update WebsiteBackendApiApplication.java

--- src/main/java/basketball/vogt/WebsiteBackendApiApplication.java
+++ src/main/java/basketball/vogt/WebsiteBackendApiApplication.java
@@ -3,6 +3,13 @@ package basketball.vogt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Fanon Jupkwo
+ * 
+ * 
+ */
+
 @SpringBootApplication
 public class WebsiteBackendApiApplication {
 
