dcd32e618cd7e1dcc2f860dd27db49d92d514cc1
Fanon Jupkwo <jupsfan@gmail.com>
2019-12-19 12:42:57 +0100

    Back to Spring Boot 2.1.8 (1)

--- src/test/java/basketball/vogt/WebsiteBackendApiApplicationTests.java
+++ src/test/java/basketball/vogt/WebsiteBackendApiApplicationTests.java
@@ -2,13 +2,16 @@ package basketball.vogt;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.junit4.SpringRunner;
 import basketball.vogt.controller.HelloController;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class WebsiteBackendApiApplicationTests {
+public class WebsiteBackendApiApplicationTests {
 
 	@Autowired
 	private HelloController helloController;
